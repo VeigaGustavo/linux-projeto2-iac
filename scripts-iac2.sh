27c64e98461d4bb32205dbafb38ddaa37b38d54a
root <root@ip-172-31-47-106.sa-east-1.compute.internal>
2025-04-26 18:53:46 +0000

    first commit

---
+++ scripts-iac2.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio.git
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
